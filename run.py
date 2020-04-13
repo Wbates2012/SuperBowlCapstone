04e73c7966ae8c40f56d60d3eee1106c5059f014
Furbeh <ufomanalien@gmail.com>
2020-04-12 23:03:43 -0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,2 +1,18 @@
+import src.ispot_scrape as ispot
+import src.ad_scraper as ad
+import sys
+import json
+
 def main():
-  return
+    argv = sys.argv
+    if 'data' in argv:
+        cfg = json.load(open('config/data-params.json'))
+        year = cfg['year']
+        superdata = cfg['superdata']
+        otherdata = cfg['otherdata']
+        datapath = cfg['datapath']
+        ispot.scrape_ispot(year, datapath, superdata)
+        ad.all_videos(year, otherdata, datapath)
+        
+if __name__ == "__main__":
+    main()
