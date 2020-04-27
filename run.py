70700e10ffef43190341686bd3adae8ad85f88e6
Wbates2012 <38802182+Wbates2012@users.noreply.github.com>
2020-04-26 21:04:08 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import src.ispot_scrape as ispot
 import src.ad_scraper as ad
+import src.ispot_clean as icl
+import src.isplot_fxt as ifx
+
 import sys
 import json
 
@@ -13,6 +16,14 @@ def main():
         datapath = cfg['datapath']
         ispot.scrape_ispot(year, datapath, superdata)
         ad.all_videos(year, otherdata, datapath)
+        
+    if 'clean' in argv:
+        cfg = json.load(open('config/clean-params.json'))
+        icl.clean(cfg['rawdatacsv'], cfg['outdir'])
+        
+    if 'fxt' in argv:
+        cfg = json.load(open('config/fxt-params.json'))
+        ifx.extract(cfg['audiodir'], cfg['cleandatadir'], cfg['outdir'])
         
 if __name__ == "__main__":
     main()
