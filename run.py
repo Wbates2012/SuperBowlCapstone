02787ee88bb552775207ae501bd55609ab459f1a
Furbeh <ufomanalien@gmail.com>
2020-06-08 14:06:40 -0700

    Add documentation to run.py, add analysis argument

--- run.py
+++ run.py
@@ -10,8 +10,18 @@ import sys
 import json
 
 def main():
+    '''
+    main executable with optional args for data
+    :arg: years: list of years to use in data testing
+    :arg: superdata: directory in datapath that holds raw superbowl data
+    :arg: otherdata: directory in datapath that holds raw non-superbowl data
+    :arg: datapath: directory of data path
+    :arg: audiodir: directory of audio files in repective datafolder
+    :arg: vizpath: path to hold vizualization outputs
+    '''
     argv = sys.argv
         
+    #Downlaod commercials and their dataframes
     if 'data' in argv:
         cfg = json.load(open('config/data-params.json'))
         years = cfg['years']
@@ -21,24 +31,30 @@ def main():
         vizpath = cfg['vizpath']
         ispot.scrape_ispot(years, datapath, superdata)
         ad.all_videos(otherdata, datapath)
-        vid.dataframe_processor(superdata, otherdata, datapath, superdata)
-        vid.dataframe_processor(superdata, otherdata, datapath, otherdata)
-        vis_vid.get_visualizations(superdata, otherdata, datapath, vizpath)
-        pred.predict(superdata, otherdata, datapath)
         
-    if 'clean' in argv:
-        cfg = json.load(open('config/clean-params.json'))
-        icl.clean(cfg['rawdatacsv'], cfg['outdir'])
-        
+    #Feature generation for both groups of commercials
     if 'fxt' in argv:
         cfg = json.load(open('config/data-params.json'))
         superdata = cfg['superdata']
         otherdata = cfg['otherdata']
         datapath = cfg['datapath']
         audiodir = cfg['audiodir']
+        vid.dataframe_processor(superdata, otherdata, datapath, superdata)
+        vid.dataframe_processor(superdata, otherdata, datapath, otherdata)
         ifx.extract(datapath, superdata, audiodir)
         ifx.extract(datapath, otherdata, audiodir)
         
+    #Analysis of features, both viz and model generation
+    if 'analyze' in argv:
+        cfg = json.load(open('config/data-params.json'))
+        superdata = cfg['superdata']
+        otherdata = cfg['otherdata']
+        datapath = cfg['datapath']
+        vizpath = cfg['vizpath']
+        vis_vid.get_visualizations(superdata, otherdata, datapath, vizpath)
+        pred.predict(superdata, otherdata, datapath)
+        
+    #Test run of project on 1 of each type of commercial
     if 'test-project' in argv:
         cfg = json.load(open('config/test-params.json'))
         years = cfg['years']
