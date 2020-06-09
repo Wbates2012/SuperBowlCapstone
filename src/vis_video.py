d7fe9526a5192cd4fdeab2bad0b254ff1f21adb5
Furbeh <ufomanalien@gmail.com>
2020-06-08 18:27:49 -0700

    Add documentation to predictor and vis_video Update predictor with separate functions

--- src/vis_video.py
+++ src/vis_video.py
@@ -5,12 +5,23 @@ import numpy as np
 
 
 def read_features(datapath, whichdata):
+    '''
+    loads feature dataframe for desired group
+    :param: datapath: directory of data path
+    :param: whichdata: which category of data to use
+    '''
     output_filename = "%s features.csv" % (whichdata)
     output_filename = os.path.join(datapath, output_filename)
     return pd.read_csv((output_filename), index_col=0, keep_default_na=False)
 
 
 def histvis(combined, feature, vispath):
+    '''
+    creates percentage histogram categorized by group
+    :param: combined: combined feature dataframe
+    :param: feature: feature to use for histogram
+    :param: vizpath: path to hold vizualization outputs
+    '''
     superbowl = combined[combined["issuperbowl"] == 1][feature]
     notsuperbowl = combined[combined["issuperbowl"] == 0][feature]
 
@@ -22,10 +33,16 @@ def histvis(combined, feature, vispath):
 
     fig.update_layout(title_text=feature.title())
     fig.write_image(os.path.join(vispath, "%s histogram.png" % (feature)))
-    # fig.show(renderer="png")
 
 
 def get_visualizations(superdata, otherdata, datapath, vizpath):
+    '''
+    wrapper for collecting all histograms for each feature
+    :param: superdata: directory in datapath that holds raw superbowl data
+    :param: otherdata: directory in datapath that holds raw non-superbowl data
+    :param: datapath: directory of data path
+    :param: vizpath: path to hold vizualization outputs
+    '''
     if not os.path.exists(vizpath):
         os.mkdir(vizpath)
     superdf = read_features(datapath, superdata)
